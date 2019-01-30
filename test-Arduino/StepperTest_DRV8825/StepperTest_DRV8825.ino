9b648dcd9053ba1d0c554a81c374550eb25ecbe2
Zilvinus-Peciulis <zilvinus.peciulis@gmail.com>
2019-01-30 22:26:47 +0200

    new

--- test-Arduino/StepperTest_DRV8825/StepperTest_DRV8825.ino
+++ test-Arduino/StepperTest_DRV8825/StepperTest_DRV8825.ino
@@ -1,27 +1,95 @@
-const static int stepPin = 3;
-const static int dirPin = 4;
+const static int stepPinX = 2;
+const static int dirPinX = 5;
+
+const static int stepPinY = 3;
+const static int dirPinY = 6;
+
+
+const static int stepPinZ = 4;
+const static int dirPinZ = 7;
+
 
 void setup() {
   // put your setup code here, to run once:
 
- pinMode (stepPin, OUTPUT);
- pinMode (dirPin, OUTPUT);
+ pinMode (stepPinX, OUTPUT);
+ pinMode (dirPinX, OUTPUT);
+
+ pinMode (stepPinX, OUTPUT);
+ pinMode (dirPinX, OUTPUT);
+
+ pinMode (stepPinX, OUTPUT);
+ pinMode (dirPinX, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalWrite (dirPin, HIGH);
+
+/*
+  digitalWrite (dirPinX, HIGH);
 
 
   for (int x = 0; x < 200; x++) {
     
-      digitalWrite (stepPin,HIGH);
+      digitalWrite (stepPinX,HIGH);
       delayMicroseconds (500);
-      digitalWrite(stepPin LOW);
+      digitalWrite(stepPinX, LOW);
       delayMicroseconds (500);
       
     }
 
 delay (1000);
-}
+
+
+digitalWrite (dirPinZ, HIGH);
+
+for (int x = 0; x < 200; x++) {
+    
+      digitalWrite (stepPinZ,HIGH);
+      delayMicroseconds (500);
+      digitalWrite(stepPinZ, LOW);
+      delayMicroseconds (500);
+      
+    }
+
+delay (1000);
+
+*/
+
+
+
+spinStepper (dirPinX, "right", stepPinX);
+delay(1000);
+spinStepper (dirPinX, "left", stepPinX);
+delay(1000);
+
+
+spinStepper (dirPinZ, "right", stepPinZ);
+delay(1000);
+spinStepper (dirPinZ, "left", stepPinZ);
+delay(1000);
+
+};
+
+
+void spinStepper (int directionPin, String direction, int stepPin) {
+
+int dir = LOW;
+
+  if (direction == "right")
+      dir = HIGH;  
+ 
+     digitalWrite (directionPin, dir);
+     
+
+for (int x = 0; x < 200; x++) {
+    
+      digitalWrite (stepPin,HIGH);
+      delayMicroseconds (500);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds (500);
+      
+    }
+    
+  }
